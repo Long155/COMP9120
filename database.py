4a74a5d9a93b8db09dd82cf89111db1662a94602
gghao78 <49826003+gghao78@users.noreply.github.com>
2024-10-18 16:16:05 +1100

    Add files via upload

---
+++ database.py
@@ -0,0 +1,272 @@
+#!/usr/bin/env python3
+import psycopg2
+
+#####################################################
+##  Database Connection
+#####################################################
+
+'''
+Connect to the database using the connection string
+'''
+def openConnection():
+    # connection parameters - ENTER YOUR LOGIN AND PASSWORD HERE
+    # userid = "y24s2c9120_jlyu0662"
+    # passwd = "pYj7CD26"
+    # myHost = "awsprddbs4836.shared.sydney.edu.au"
+    database = "postgres"
+    userid = "postgres"
+    passwd = "123123"
+    myHost = "localhost"
+    myPort = "5432"
+
+
+    # Create a connection to the database
+    conn = None
+    try:
+        # Parses the config file and connects using the connect string
+        # conn = psycopg2.connect(database=userid,
+        #                             user=userid,
+        #                             password=passwd,
+        #                             host=myHost)
+        conn = psycopg2.connect(database=database, user=userid, password=passwd, host=myHost, port=myPort) #local
+
+    except psycopg2.Error as sqle:
+        error_message = sqle.pgerror if sqle.pgerror else "Unknown error"
+        print("psycopg2.Error : " + error_message)
+    
+    # return the connection to use
+    return conn
+
+'''
+Validate staff based on username and password
+'''
+def checkLogin(login, password):
+    # Connect Database
+    conn = openConnection()
+    if conn is None:
+        return None
+    
+    cursor = conn.cursor()
+    try:
+        # We use Cursor to help us to serach user infromation
+        cursor.execute(
+            '''
+                SELECT username, firstname, lastname, email
+                FROM  administrator
+                WHERE username = %s And password = %s
+            '''
+            ,(login, password)
+        )
+        user = cursor.fetchall()
+        
+        # If we can not find userid , return NULL
+        if not user:
+            return None
+        
+        print('User: ',user)
+        print('user(flatten):', list(user[0]))
+        
+        user_info = list(user[0])
+        return user_info
+        
+    except psycopg2.Error as sqle:
+        print("psycopg2.Error : " + sqle.pgerror)
+        return None
+    finally:
+        cursor.close()
+        conn.close()
+
+'''
+List all the associated admissions records in the database by staff
+'''
+def findAdmissionsByAdmin(login):
+
+    # Connect Database
+    conn = openConnection()
+    if conn is None:
+        return None
+    
+    cursor = conn.cursor()
+    try:
+        # We use Cursor to help us to serach user infromation
+        cursor.execute(
+            '''
+                Select A.admissionid AS admission_id, 
+                       ATE.admissiontypename AS admission_type,
+                       D.deptname AS admission_department ,
+		               COALESCE(TO_CHAR(A.dischargedate,'DD-MM-YYYY'),'') as discharge_date,
+		               COALESCE(A.fee :: TEXT,'') as fee,
+	                   CONCAT(p.firstname,' ' ,p.lastname) AS patient,
+	                   COALESCE(A.condition,'') as condition
+                FROM admission A
+                JOIN patient P ON A.patient = P.patientid
+                Join admissiontype ATE ON A.admissiontype = ATE.admissiontypeid 
+                JOIN department D ON A.department = D.deptid
+                WHERE A.administrator = 'jdoe'
+                ORDER BY A.dischargedate DESC NULLS LAST,
+		                 CONCAT(p.firstname,' ' ,p.lastname) ASC,
+		                 ATE.admissiontypename DESC
+            '''
+            ,(login,)
+        )
+        rows = cursor.fetchall()
+        
+        # If we can not find rows , return NULL
+        if not rows:
+            return None
+        
+        print('rows: ',rows)
+        # print('user(flatten):', list(user[0]))
+        attributes = [attr[0] for attr in cursor.description]
+        print(attributes)
+        row_to_dict = [dict(zip(attributes,row)) for row in rows]
+        print(row_to_dict)
+        return row_to_dict
+        
+    except psycopg2.Error as sqle:
+        print("psycopg2.Error : " + sqle.pgerror)
+        return None
+    finally:
+        cursor.close()
+        conn.close()
+
+
+'''
+Find a list of admissions based on the searchString provided as parameter
+See assignment description for search specification
+'''
+def findAdmissionsByCriteria(searchString):
+# Connect Database
+    conn = openConnection()
+    if conn is None:
+        return None
+    
+    cursor = conn.cursor()
+    try:
+        # Use Cursor to find admission records that meet the criteria
+        query = '''
+            SELECT A.admissionid AS admission_id, 
+                   ATE.admissiontypename AS admission_type,
+                   D.deptname AS admission_department,
+                   COALESCE(TO_CHAR(A.dischargedate,'DD-MM-YYYY'),'') AS discharge_date,
+                   COALESCE(A.fee :: TEXT,'') AS fee,
+                   CONCAT(p.firstname, ' ', p.lastname) AS patient,
+                   COALESCE(A.condition,'') AS condition
+            FROM admission A
+            JOIN patient P ON A.patient = P.patientid
+            JOIN admissiontype ATE ON A.admissiontype = ATE.admissiontypeid
+            JOIN department D ON A.department = D.deptid
+            WHERE LOWER(ATE.admissiontypename) LIKE %s 
+               OR LOWER(D.deptname) LIKE %s 
+               OR LOWER(CONCAT(p.firstname, ' ', p.lastname)) LIKE %s 
+               OR LOWER(A.condition) LIKE %s
+            AND (A.dischargedate IS NULL OR A.dischargedate > CURRENT_DATE - INTERVAL '2 years')
+            ORDER BY A.dischargedate ASC NULLS FIRST,
+                     CONCAT(p.firstname, ' ', p.lastname) ASC
+        '''
+        searchString = f"%{searchString.lower()}%"
+        cursor.execute(query, (searchString, searchString, searchString, searchString))
+        rows = cursor.fetchall()
+
+        if not rows:
+            return None
+
+        attributes = [attr[0] for attr in cursor.description]
+        row_to_dict = [dict(zip(attributes, row)) for row in rows]
+        return row_to_dict
+
+    except psycopg2.Error as sqle:
+        print("psycopg2.Error : " + sqle.pgerror)
+        return None
+    finally:
+        cursor.close()
+        conn.close()
+    return
+
+
+'''
+Add a new addmission 
+'''
+def addAdmission(type, department, patient, condition, admin):
+     # Connect to Database
+    conn = openConnection()
+    if conn is None:
+        return False
+
+    cursor = conn.cursor()
+    try:
+        # Using nested SELECT statements to insert new admission records
+        query = '''
+            INSERT INTO admission (admissiontype, department, patient, condition, administrator)
+            VALUES (
+                (SELECT admissiontypeid FROM admissiontype WHERE LOWER(admissiontypename) = LOWER(%s)),
+                (SELECT deptid FROM department WHERE LOWER(deptname) = LOWER(%s)),
+                %s,
+                %s,
+                %s
+            );
+        '''
+        cursor.execute(query, (type, department, patient, condition, admin))
+        conn.commit()
+        print("Admission added successfully")
+        return True
+
+    except psycopg2.Error as sqle:
+        print("psycopg2.Error: ", sqle.pgerror)
+        print("Error Details: ", sqle.diag.message_primary)
+        return False
+    finally:
+        cursor.close()
+        conn.close()
+    return
+
+
+'''
+Update an existing admission 
+'''
+def updateAdmission(id, type, department, dischargeDate, fee, patient, condition): #可进一步增加容错性
+    conn = openConnection()
+    cursor = conn.cursor()
+    
+    try:
+        if isinstance(type, str):#presume type is either str or int
+            print("***type is str!")
+            cursor.execute("SELECT AdmissionTypeID FROM AdmissionType WHERE LOWER(AdmissionTypeName) = LOWER(%s)", (type,))
+            admission_type_id = cursor.fetchone()
+            print("***admission_type_id = cursor.fetchone() =", admission_type_id)
+            if admission_type_id is None:
+                raise ValueError("Invalid Admission Type")
+            type = admission_type_id[0]
+        
+        
+        print(f"***department = {department}")
+        print("***fee=", fee)
+        if isinstance(department, str):
+            print("***department is str!")
+            cursor.execute("SELECT DeptId FROM Department WHERE LOWER(DeptName) = LOWER(%s)", (department,))
+            department_id = cursor.fetchone()
+            print("***department_id = cursor.fetchone() =", department_id)
+            if department_id is None:
+                raise ValueError("Invalid Department")
+            department = department_id[0]
+        
+        patient_lower = patient.lower()
+        cursor.execute("""
+            UPDATE Admission
+            SET AdmissionType = %s, Department = %s, DischargeDate = %s, Fee = %s, Patient = %s, Condition = %s
+            WHERE AdmissionID = %s
+        """, (int(type), int(department), dischargeDate, fee, patient_lower, condition, id))
+        
+        conn.commit()
+        return cursor.rowcount > 0
+    except psycopg2.Error as e:
+        print("Error updating admission: ", e)
+        conn.rollback()
+        return False
+    except ValueError as ve:
+        print(ve)
+        return False
+    finally:
+        cursor.close()
+        conn.close()
+
